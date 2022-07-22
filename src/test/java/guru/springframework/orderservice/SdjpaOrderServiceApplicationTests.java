afea719289f300ff706d25dac488edf19673ec39
PierreSQS <nomo_messina@web.de>
2022-07-22 18:16:53 +0200

    Sect16_Chap132-One to Many-INC: Pierrot DB settings

--- src/test/java/guru/springframework/orderservice/SdjpaOrderServiceApplicationTests.java
+++ src/test/java/guru/springframework/orderservice/SdjpaOrderServiceApplicationTests.java
@@ -3,13 +3,17 @@ package guru.springframework.orderservice;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Disabled // not configured...
 @SpringBootTest
 class SdjpaOrderServiceApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads(ApplicationContext appCtx) {
+        assertThat(appCtx).isNotNull();
     }
 
 }
