6a84c65420082c63199961448b9799a56f805840
PierreSQS <nomo_messina@web.de>
2022-07-24 23:19:20 +0200

    Sect16_Ass19-Create Customer-INC: added a new Entity

---
+++ src/main/java/guru/springframework/orderservice/domain/Customer.java
@@ -0,0 +1,47 @@
+package guru.springframework.orderservice.domain;
+
+import jakarta.persistence.Entity;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Customer extends BaseEntity {
+    private String contactInfo;
+
+    private Set<OrderHeader> orderHeaders;
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public Set<OrderHeader> getOrderHeaders() {
+        return orderHeaders;
+    }
+
+    public void setOrderHeaders(Set<OrderHeader> orderHeaders) {
+        this.orderHeaders = orderHeaders;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        Customer customer = (Customer) o;
+
+        return Objects.equals(contactInfo, customer.contactInfo);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (contactInfo != null ? contactInfo.hashCode() : 0);
+        return result;
+    }
+}
