182e0b3609d1a5135380aee34530f124695149ac
PierreSQS <nomo_messina@web.de>
2022-07-26 08:56:44 +0200

    Sect16_Chap140-Orphan removal: remove the approval orphan after order deletion

--- src/main/java/guru/springframework/orderservice/domain/OrderHeader.java
+++ src/main/java/guru/springframework/orderservice/domain/OrderHeader.java
@@ -58,7 +58,7 @@ public class OrderHeader extends BaseEntity {
     @ManyToOne(targetEntity = Customer.class)
     private Customer customer;
 
-    @OneToOne(cascade = CascadeType.PERSIST)
+    @OneToOne(cascade = CascadeType.PERSIST, orphanRemoval = true)
     @JoinColumn(name = "order_approval_id")
     private OrderApproval orderApproval;
 
