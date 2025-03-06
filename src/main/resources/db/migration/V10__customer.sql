2300e02e24f64e741c47af3ba6260b1137e4c9c3
PierreSQS <nomo_messina@web.de>
2025-03-06 17:04:11 +0100

    Sec17_Chap144-XXX: fixed the previous issue(642cfc14)

--- src/main/resources/db/migration/V10__customer.sql
+++ src/main/resources/db/migration/V10__customer.sql
@@ -29,5 +29,5 @@ insert into customer (customer_name, address, city, state, zip_code, phone, emai
     values ('Customer 2', '124 Rue Bell', 'Douala', 'Wouri', '42421', '307.302.3285',
             'cheeseburger@margaritville.com' );
 
-update order_header set order_header.customer_id = (select id from customer limit 1);
---update order_header set order_header.customer_id = (select id from customer where id = 2);
+-- update order_header set order_header.customer_id = (select id from customer limit 1);
+update order_header set order_header.customer_id = (select id from customer where id = 2);
