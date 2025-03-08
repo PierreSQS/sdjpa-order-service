7809e9a39a8cdf7c7fc72b33db895ac0b45698bb
PierreSQS <nomo_messina@web.de>
2025-03-08 09:16:23 +0100

    Sec18_Chap155: added Version property to Customer

--- src/main/java/guru/springframework/orderservice/bootstrap/Bootstrap.java
+++ src/main/java/guru/springframework/orderservice/bootstrap/Bootstrap.java
@@ -1,5 +1,8 @@
 package guru.springframework.orderservice.bootstrap;
 
+import guru.springframework.orderservice.domain.Customer;
+import guru.springframework.orderservice.repositories.CustomerRepository;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,14 +12,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class Bootstrap implements CommandLineRunner {
 
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(Bootstrap.class);
+
     private final BootstrapOrderService bootstrapOrderServ;
 
-    public Bootstrap(BootstrapOrderService bootstrapOrderServ) {
+    private final CustomerRepository customerRepo;
+
+    public Bootstrap(BootstrapOrderService bootstrapOrderServ, CustomerRepository customerRepo) {
         this.bootstrapOrderServ = bootstrapOrderServ;
+        this.customerRepo = customerRepo;
     }
 
     @Override
     public void run(String... args) {
         bootstrapOrderServ.readOrderHeader();
+
+        Customer customer = new Customer();
+        customer.setCustomerName("Pierrot");
+        Customer savedCustomer = customerRepo.save(customer);
+
+        // log the saved customer version
+        log.info("### The Customer Version: {} ###", savedCustomer.getVersion());
+
+        // delete the saved customer
+        customerRepo.delete(savedCustomer);
+
     }
 }
--- src/main/java/guru/springframework/orderservice/domain/Customer.java
+++ src/main/java/guru/springframework/orderservice/domain/Customer.java
@@ -4,6 +4,8 @@ package guru.springframework.orderservice.domain;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Version;
+
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -20,6 +22,9 @@ public class Customer extends BaseEntity {
 
     private String phone;
     private String email;
+
+    @Version
+    private Integer version;
 
     @OneToMany(mappedBy = "customer")
     private Set<OrderHeader> orders = new LinkedHashSet<>();
@@ -62,5 +67,13 @@ public class Customer extends BaseEntity {
 
     public void setOrders(Set<OrderHeader> orders) {
         this.orders = orders;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
     }
 }
